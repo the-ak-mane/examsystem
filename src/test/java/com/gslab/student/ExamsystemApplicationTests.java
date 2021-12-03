d35ac3fc9920a8b38bc601cd48572b2ad74fd8c0
Akshay S Mane <GS-2218@GSLAB.COM>
2021-12-04 03:48:59 +0530

    first commit to git

---
+++ src/test/java/com/gslab/student/ExamsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gslab.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
